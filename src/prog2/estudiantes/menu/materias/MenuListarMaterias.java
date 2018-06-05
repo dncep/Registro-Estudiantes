3305895cd1c6fcb6b8cc609cd2165c8013bec3d3
dncep <d.cepeda256@gmail.com>
2018-06-05 16:20:35 -0400

    Agregar Listar Materia

--- src/prog2/estudiantes/menu/materias/MenuListarMaterias.java
+++ src/prog2/estudiantes/menu/materias/MenuListarMaterias.java
@@ -1,5 +1,6 @@
 package prog2.estudiantes.menu.materias;
 
+import prog2.estudiantes.data.Materia;
 import prog2.estudiantes.data.Registro;
 import prog2.estudiantes.menu.Menu;
 
@@ -13,6 +14,14 @@ public class MenuListarMaterias implements Menu {
 
     @Override
     public boolean seleccionar(Registro registro, Scanner scanner) {
+        if(registro.materias.isEmpty()) {
+            System.out.println("No hay materias registradas");
+        } else {
+            for(Materia materia : registro.materias) {
+                System.out.println(materia.codigo + " - " + materia.nombre);
+                System.out.println("\tID: " + materia.id + " | Área: " + materia.area.getNombre());
+            }
+        }
         return true;
     }
 }
